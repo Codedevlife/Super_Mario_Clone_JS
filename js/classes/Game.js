af60a7a11d4cc9528aa4bc24296f6fe3bc77b014
Codedevlife <code.dev.life@gmail.com>
2026-02-17 12:55:29 -0300

    Fim do refatoramento. Daresmos continuida de brench master/main

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -14,13 +14,18 @@ class Game{
     }
 
     start(){
-        this.player = new Player(this.ctx);
+        this.player = new Player(this.ctx, 0, 0, 150, 150);
         this.animationLoop();
     }
     
     animationLoop(){
         this.ctx.clearRect(0, 0, this.width, this.heith);
-        console.log('Gameloop iniciado')
+        this.ctx.fillStyle = 'skyblue';
+        this.ctx.fillRect(0, 0, this.width, this.heith);
+
+        this.player.draw();
+        this.player.animation();
+
         requestAnimationFrame(()=>this.animationLoop());
     }
 }
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -5,12 +5,18 @@ class Player{
         this.postionY = py;
         this.width = w;
         this.height = h;
+
+        this.marioSprite = new Image();
+        this.marioSprite.src = '../../img/Sprites/mario.png';
     }
 
     draw(){
-        this.ctx.fillRect = "red";
-        
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(this.postionX, this.postionY, this.width, this.height)
     }
-}
+
+    animation(){
+        this.ctx.drawImage(this.marioSprite, 0, 0, 16, 30, this.postionX, this.postionY, this.width, this.height);
+    }}
 
 export default Player;
