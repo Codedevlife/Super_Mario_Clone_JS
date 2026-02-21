be9f1086fab1999ddab3d1d91288e22f1c1d7f79
Codedevlife <code.dev.life@gmail.com>
2026-02-20 22:37:47 -0300

    Feito atualização para implementar o deltaTime do jogo

--- js/main.js
+++ js/main.js
@@ -4,10 +4,29 @@ const ctx = canvas.getContext('2d');
 const GAME_WIDTH = canvas.width = 800;
 const GAME_HEIGHT = canvas.height = 600;
 
-function loop(){
+let lastTime = 0;
 
-    console.log('teste')
-    requestAnimationFrame(loop)
+function loop(currentTime) {
+    // 1. Calcula a diferença em milissegundos
+    let deltaTimeMS = currentTime - lastTime;
+    
+    // 2. Converte para segundos (ex: 0.016 para 60 FPS)    
+    let deltaTime = deltaTimeMS / 1000;
+
+    // 3. Atualiza o lastTime para o próximo frame
+    lastTime = currentTime;
+
+    // 4. Proteção 
+    // Se o deltaTime for maior que 0.1s, tratamos como 0 para o jogo não travar
+    if (deltaTime > 0.1) deltaTime = 0;
+
+    // --- lógica ---
+    // update(deltaTime);
+    // draw();
+    console.log(deltaTime);
+    
+    requestAnimationFrame(loop);
 }
 
-loop();
+// Inicia o loop
+requestAnimationFrame(loop);
